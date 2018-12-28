108ccefb496d4d3ccc1fdea022b273b63474ad04
米田大弥 <yoneda.dayone8@gmail.com>
2018-12-29 01:08:19 +0900

    fixed 2.1.16 の課題が終了しました

---
+++ stv_project_2.1.16/stv_project_2.1.16/ViewController1.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController1.swift
+//  stv_project_2.1.16
+//
+//  Created by 米田大弥 on 2018/12/23.
+//  Copyright © 2018年 hiroya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ stv_project_2.1.16/stv_project_2.1.16/ViewController3.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController3.swift
+//  stv_project_2.1.16
+//
+//  Created by 米田大弥 on 2018/12/23.
+//  Copyright © 2018年 hiroya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
