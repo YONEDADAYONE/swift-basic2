311f0fd39c8363c7b94040964631ac60a440c891
米田大弥 <yoneda.dayone8@gmail.com>
2018-12-29 01:20:57 +0900

    fixed 2.1.17 の課題が終了しました

---
+++ stv_project_2.1.17/stv_project_2.1.17/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  stv_project_2.1.17
+//
+//  Created by 米田大弥 on 2018/12/24.
+//  Copyright © 2018年 hiroya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func onClickMyButton(sender: UIButton) {
+        // 遷移するViewを定義する。
+        let secondViewController: SecondViewController =
+            self.storyboard?.instantiateViewController(withIdentifier: "segue")
+                as? SecondViewController ?? SecondViewController()
+        // Viewの移動する.
+        self.present(secondViewController, animated: true, completion: nil)
+    }
+    
+}
