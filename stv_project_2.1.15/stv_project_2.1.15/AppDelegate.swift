99d6c12490bf7dec611053e4478ffe830c07389f
米田大弥 <yoneda.dayone8@gmail.com>
2018-12-29 01:03:09 +0900

    fixed 2.1.15 の課題が終了しました

---
+++ stv_project_2.1.15/stv_project_2.1.15/AppDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  AppDelegate.swift
+//  stv_project_2.1.15
+//
+//  Created by 米田大弥 on 2018/12/22.
+//  Copyright © 2018年 hiroya. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+
+}
---
+++ stv_project_2.1.15/stv_project_2.1.15/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  stv_project_2.1.15
+//
+//  Created by 米田大弥 on 2018/12/22.
+//  Copyright © 2018年 hiroya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
