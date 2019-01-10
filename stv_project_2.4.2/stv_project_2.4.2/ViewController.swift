5a3e8152ef63254a7d675f2f00e684b57cc41826
米田大弥 <yoneda.dayone8@gmail.com>
2019-01-10 12:37:35 +0900

    fixed 2.4.2の課題が終了しました

--- stv_project_2.4.2/stv_project_2.4.2/ViewController.swift
+++ stv_project_2.4.2/stv_project_2.4.2/ViewController.swift
@@ -6,10 +6,7 @@
 //  Copyright © 2019 hiroya. All rights reserved.
 //
 
-
 import UIKit
-import Alamofire
-import SwiftyJSON
 
 class ViewController: UIViewController {
     
@@ -18,97 +15,47 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        Forecaster.forecast { (result) in
+        Forecaster.forecast { result in
             self.forecasts = result.forecasts
             print(self.forecasts)
         }
+        
     }
     
-    
+    @IBAction func actionButton(_ sender: UIButton) {
+        
+        let alert: UIAlertController =
+            UIAlertController(title: "天気です",
+                              message: "どのデータを閲覧しますか？",
+                              preferredStyle: UIAlertController.Style.actionSheet)
+        
+        let facebookActionsheet: UIAlertAction =
+            UIAlertAction(title: "本日",
+                          style: UIAlertAction.Style.default,
+                          handler: {(_: UIAlertAction) in
+                            print(self.forecasts[0])
+            })
+        
+        let twitterActionsheet: UIAlertAction =
+            UIAlertAction(title: "明日",
+                          style: UIAlertAction.Style.default,
+                          handler: {(_: UIAlertAction) in
+                            print(self.forecasts[1])
+            })
+        
+        let lineActionsheet: UIAlertAction =
+            UIAlertAction(title: "明後日",
+                          style: UIAlertAction.Style.default,
+                          handler: {(_: UIAlertAction) in
+                            print(self.forecasts[2])
+            })
+        
+        alert.addAction(facebookActionsheet)
+        alert.addAction(twitterActionsheet)
+        alert.addAction(lineActionsheet)
+        
+        self.present(alert, animated: true, completion: nil)
+        
     }
     
-//    func getweather() {
-//        //お天気APIから東京の天気を取得する
-//        Alamofire.request("http://weather.livedoor.com/forecast/webservice/json/v1?city=130010").responseJSON {response in
-//
-//            let json: JSON = JSON(response.value ?? kill) //killは三項演算子
-//
-//            let forecasts = json["forecasts"]
-//            let image = forecasts["dateLabel"]
-////            print(forecasts)
-////            print("image = \(image)")
-//
-//        //ここからアクションシート
-//            let alert: UIAlertController =
-//                UIAlertController(title: "お天気情報です",
-//                                  message: "どれにしますか？",
-//                                  preferredStyle: UIAlertController.Style.actionSheet)
-//
-//            let todayActionsheet: UIAlertAction =
-//                UIAlertAction(title: "今日",
-//                              style: UIAlertAction.Style.default,
-//                              handler: {(_: UIAlertAction) in
-//                //ここに処理を記載(今日の天気の情報)
-//
-//                })
-//}
-//}
-
-
-
-
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        //天気のAPIにアクセスし、APIの結果をJSONで取得
-//        let str : String = "http://weather.livedoor.com/forecast/webservice/json/v1?city=130010"
-//        let url : URL = URL(string: str)!
-//        // URLRequestを生成してJSONのデータを取得
-//        let request: URLRequest = URLRequest(url:url)
-//        let session = URLSession.shared
-//        let task : URLSessionDataTask = session.dataTask(with: request, completionHandler: {(data, response, error) in
-//
-//            // APIからの戻り値がなければ処理を終了
-//            guard let data = data else{ return }
-//
-//            // エラーがあれば表示
-//            if(error != nil) {
-//                print(error ?? "")
-//                return
-//            }
-//            print(data)
-//            do{
-//                // JSONDecoderクラスのインスタンスを生成
-//                let decoder = JSONDecoder()
-//                // JSONを解析して作成した構造体の通りにマッピング
-//                let resultList = try? decoder.decode(ResultList.self, from: data)
-//                print(resultList)
-//
-////                // JSONを解析した後、User構造体にマッピングされたデータを取り出す
-////                for user in (resultList?.results)! {
-////                }
-//
-//            }catch{
-//                print("JSONの解析でエラーが起きました")
-//            }
-//        })
-//        task.resume()
-//    }
-//
-//    // resultsを格納する構造体
-//    struct ResultList:Codable{
-//        let results : [Weather]
-//    }
-//
-//    // ユーザー情報を格納す構造体
-//    struct Weather: Codable  {
-//
-//        let forecasts: Name
-//        struct Name: Codable  {
-//            let datelabel: String
-//            let telop: String
-//            let date: String
-//        }
-//    }
-//
-//}
+    }
--- stv_project_2.4.2/stv_project_2.4.2/Weather.swift
+++ stv_project_2.4.2/stv_project_2.4.2/Weather.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class Forecaster: NSObject {
     
-    static func forecast(completion:@escaping (ForecastResult)->Void){
+    static func forecast(completion: @escaping (ForecastResult) -> Void) {
         
         let urlString = "http://weather.livedoor.com/forecast/webservice/json/v1?city=130010"
         guard let url = URL(string: urlString) else {
@@ -18,7 +18,9 @@ class Forecaster: NSObject {
             return
         }
         
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+        // let task = URLSession.shared.dataTask(with: url) { data, response, error in にしていたところswiftlintに文句を
+        //言われたので responseを_に変更。
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
             
             guard let jsonData = data else {
                 print("JSON data error")
@@ -26,24 +28,27 @@ class Forecaster: NSObject {
             }
             
             do {
-                let result:ForecastResult = try JSONDecoder().decode(ForecastResult.self, from: jsonData)
-                print("restgrdgd456754",result)
+                let result: ForecastResult = try JSONDecoder().decode(ForecastResult.self, from: jsonData)
+                print("エラーがなければ結果は→", result)
                 completion(result)
-            }catch let error{
-                print("Error", error)
+            } catch let error {
+                print("エラーがなあればここで止まる。", error)
             }
         }
         task.resume()
     }
 }
 
+//最重要　Codableを利用する際は必ず変数名はAPIのサイト通りに必ずしなければならない！
 struct ForecastResult: Codable {
-    var forecasts:[ForecastList]
+    //超重要　変数名はAPIのサイト通りに必ずしなければならない
+    var forecasts: [ForecastList]
 }
 
+//区別するためにForecastにListを加え、ForecastListにした
 struct ForecastList: Codable {
-    var dateLabel:String
-    var telop:String
-    var date:String
+    //超重要 この変数も必ずAPIのサイト通りにすること!
+    var dateLabel: String
+    var telop: String
+    var date: String
 }
-
