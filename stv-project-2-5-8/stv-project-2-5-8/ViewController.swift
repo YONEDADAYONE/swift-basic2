f7fa6fad7b73c2f53115b32309897cc8380f9e65
米田大弥 <yoneda.dayone8@gmail.com>
2019-01-17 16:13:18 +0900

    stv_project_2.5.8プッシュ機能導入前

---
+++ stv-project-2-5-8/stv-project-2-5-8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stv-project-2-5-8
+//
+//  Created by 米田大弥 on 2019/01/17.
+//  Copyright © 2019 hiroya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
