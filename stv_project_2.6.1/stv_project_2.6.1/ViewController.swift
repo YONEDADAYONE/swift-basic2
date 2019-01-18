47e314ffde561a2462d0aa901209dc8de3231f0e
米田大弥 <yoneda.dayone8@gmail.com>
2019-01-18 15:00:17 +0900

    fixd_stv_project_2.6.1swiftLintを導入しました

---
+++ stv_project_2.6.1/stv_project_2.6.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stv_project_2.6.1
+//
+//  Created by 米田大弥 on 2019/01/17.
+//  Copyright © 2019 hiroya. All rights reserved.
+//
+
+import UIKit
+
+//https://dev.classmethod.jp/smartphone/ios-custom-url-scheme/を参照
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
