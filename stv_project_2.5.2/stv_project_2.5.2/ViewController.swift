818aa28240cd3577ef52209a112b173ab27327b7
米田大弥 <yoneda.dayone8@gmail.com>
2019-01-21 20:15:32 +0900

    fixd_stv_project_2.5.2訂正致しました。

--- stv_project_2.5.2/stv_project_2.5.2/ViewController.swift
+++ stv_project_2.5.2/stv_project_2.5.2/ViewController.swift
@@ -9,32 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
-UINavigationControllerDelegate{
+UINavigationControllerDelegate {
     
-    @IBOutlet var cameraView : UIImageView!
+    @IBOutlet private var cameraView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
     }
     
     // カメラの撮影開始
-    @IBAction func startCamera(_ sender : AnyObject) {
+    @IBAction func startCamera(_ sender: AnyObject) {
         
-        let sourceType:UIImagePickerController.SourceType =
+        let sourceType: UIImagePickerController.SourceType =
             UIImagePickerController.SourceType.camera
         // カメラが利用可能かチェック
         if UIImagePickerController.isSourceTypeAvailable(
-            UIImagePickerController.SourceType.camera){
+            UIImagePickerController.SourceType.camera) {
             // インスタンスの作成
             let cameraPicker = UIImagePickerController()
             cameraPicker.sourceType = sourceType
             cameraPicker.delegate = self
             self.present(cameraPicker, animated: true, completion: nil)
-            
-        }
-        else{
+        } else {
             print("エラー")
             
         }
@@ -42,19 +38,18 @@ UINavigationControllerDelegate{
     
     //　撮影が完了時した時に呼ばれる
     func imagePickerController(_ imagePicker: UIImagePickerController,
-                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
         
         if let pickedImage = info[.originalImage]
             as? UIImage {
             
             cameraView.contentMode = .scaleAspectFit
             cameraView.image = pickedImage
-            
         }
         
         //閉じる処理
         imagePicker.dismiss(animated: true, completion: nil)
-        print("エラー")
+        print("閉じます")
         
     }
     
@@ -64,57 +59,40 @@ UINavigationControllerDelegate{
         print("エラー")
     }
     
-    
-    // 写真を保存
-//    @IBAction func savePicture(_ sender : AnyObject) {
-//        let image:UIImage! = cameraView.image
-//
-//        if image != nil {
-//            UIImageWriteToSavedPhotosAlbum(
-//                image,
-//                self,
-//                #selector(ViewController.image(_:didFinishSavingWithError:contextInfo:)),
-//                nil)
-//        }
-//        else{
-//           print("エラー")
-//        }
-//
-//    }
-    
     // 書き込み完了結果の受け取り
     @objc func image(_ image: UIImage,
-                     didFinishSavingWithError error: NSError!,
+                     didFinishSavingWithError error: NSError?,
                      contextInfo: UnsafeMutableRawPointer) {
         
         if error != nil {
-            print(error.code)
-        }
-        else{
-            print("成功")
+            print(error?.code as Any)
+            print("書き込み失敗")
+        } else {
+            print("書き込み成功")
         }
     }
     
-//    // アルバムを表示
-//    @IBAction func showAlbum(_ sender : AnyObject) {
-//        let sourceType:UIImagePickerController.SourceType =
-//            UIImagePickerController.SourceType.photoLibrary
-//
-//        if UIImagePickerController.isSourceTypeAvailable(
-//            UIImagePickerController.SourceType.photoLibrary){
-//            // インスタンスの作成
-//            let cameraPicker = UIImagePickerController()
-//            cameraPicker.sourceType = sourceType
-//            cameraPicker.delegate = self
-//            self.present(cameraPicker, animated: true, completion: nil)
-//
-//            print("save")
-//        }
-//        else{
-//            print("エラー")
-//
-//        }
-//
-//    }
-    
+    @IBAction func monochromeButton(_ sender: UIButton) {
+        //CIFilterの導入
+        
+        let filteredImage = CIImage(image: self.cameraView.image ?? UIImage())
+            let filter = CIFilter(name: "CIVignette")
+            
+            filter?.setValue(filteredImage, forKey: "inputImage")
+            filter?.setValue(NSNumber(value: 9.0), forKey: "inputIntensity")
+        
+            let ciContext = CIContext(options: nil)
+        
+        guard let imageRef = ciContext.createCGImage((filter?.outputImage ?? CIImage()),
+                                                     from: (filter?.outputImage?.extent ?? CGRect())) else {
+            return
+        }
+        //空の配列todoArrayにrelmeのデータを入れてフィルターをかける
+        
+        do {
+            let outputImage = UIImage(cgImage: imageRef)
+            self.cameraView.image = outputImage
+        }
+        
+        }
 }
