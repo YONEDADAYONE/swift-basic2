1944144f0675beedc129a764b66a104931f70939
米田大弥 <yoneda.dayone8@gmail.com>
2019-01-22 20:41:26 +0900

    fix_stv_project_2.4.4realm導入前

--- stv_project_2.4.4/stv_project_2.4.4/weather.swift
+++ stv_project_2.4.4/stv_project_2.4.4/weather.swift
@@ -43,24 +43,18 @@ class Forecaster: NSObject {
 struct ForecastResult: Codable {
     //超重要　変数名はAPIのサイト通りに必ずしなければならない
     var forecasts: [ForecastList]
-    var description: DescriptionList
-    
 }
 
 //区別するためにForecastにListを加え、ForecastListにした
 struct ForecastList: Codable {
     //超重要 この変数も必ずAPIのサイト通りにすること!
-    var dateLabel: String //日付
-    var date:String //予報日
-    var telop:String //天気
-    var image:ImageList
+    var date: String //予報日
+    var telop: String //天気
+    var image: Image
+    
+    //当初のクラス名はImageの「I」が小文字の「i」だったがswiftLintの指摘により訂正
+    struct Image: Codable {
+        var url: String
+    }
+    
 }
-
-struct ImageList: Codable {
-    var url:String
-}
-
-struct DescriptionList: Codable {
-    var text:String
-}
-
