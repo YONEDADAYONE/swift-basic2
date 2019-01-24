dfb2955f562e6053895a7eac3bd56de8791dbb0e
米田大弥 <yoneda.dayone8@gmail.com>
2019-01-24 18:34:52 +0900

    first commit

---
+++ stv_project_2.4.5/stv_project_2.4.5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stv_project_2.4.5
+//
+//  Created by 米田大弥 on 2019/01/24.
+//  Copyright © 2019 hiroya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
