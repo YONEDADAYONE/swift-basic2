a58e1e66e39a928b688d36ae66ab7ee1d467e832
米田大弥 <yoneda.dayone8@gmail.com>
2019-01-29 10:48:06 +0900

    firstcommit_Calendarアプリストーリーボード設定。

---
+++ Calendar/Calendar/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Calendar
+//
+//  Created by 米田大弥 on 2019/01/29.
+//  Copyright © 2019 hiroya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    @IBOutlet weak var zengetsuButton: UIBarButtonItem!
+    @IBOutlet weak var jigetsuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
