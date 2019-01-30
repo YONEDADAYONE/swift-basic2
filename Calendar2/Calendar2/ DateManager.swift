a37f0c303ac835f1559b81100ba35688cd74b550
米田大弥 <yoneda.dayone8@gmail.com>
2019-01-30 13:06:36 +0900

    ボタンが正常に動くことを確認

---
+++ Calendar2/Calendar2/ DateManager.swift
@@ -0,0 +1,117 @@
+//
+//   DateManager.swift
+//  Calendar2
+//
+//  Created by 米田大弥 on 2019/01/30.
+//  Copyright © 2019 hiroya. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+extension Date {
+
+    func monthAgoDate() -> Date {
+        let addValue = -1
+
+        let calendar = Calendar.current
+
+        var dateComponents = DateComponents()
+        dateComponents.month = addValue
+
+        return calendar.date(byAdding: dateComponents, to: self)!
+    }
+
+    func monthLaterDate() -> Date {
+        let addValue: Int = 1
+
+        let calendar = Calendar.current
+
+        var dateComponents = DateComponents()
+        dateComponents.month = addValue
+        
+        return calendar.date(byAdding: dateComponents, to: self)!
+    }
+}
+
+class DateManager: NSObject {
+    
+    var currentMonthOfDates = [NSDate]() //表記する月の配列
+
+    var selectedDate = Date()
+    let daysPerWeek: Int = 7
+
+    var numberOfItems: Int! = 0 //セルの個数 nilが入らないようにする
+    
+    //月ごとのセルの数を返すメソッド
+    func daysAcquisition() -> Int {
+
+        let rangeOfWeeks = Calendar.current.range(of: .weekOfMonth, in: .month, for: firstDateOfMonth() as Date)
+        
+        let numberOfWeeks = Int((rangeOfWeeks?.count)!) //月が持つ週の数
+        numberOfItems = numberOfWeeks * daysPerWeek //週の数×列の数
+        return numberOfItems
+    }
+    //月の初日を取得
+    func firstDateOfMonth() -> Date {
+
+        var components = Calendar.current.dateComponents([.year, .month, .day], from:selectedDate)
+        components.day = 1
+
+        let firstDateMonth = Calendar.current.date(from: components)!
+        return firstDateMonth
+    }
+    
+    // ⑴表記する日にちの取得
+    func dateForCellAtIndexPath(numberOfItem: Int) {
+        // ①「月の初日が週の何日目か」を計算する
+        // 🔴修正前 NSCalendar.currentCalendar().ordinalityOfUnit(NSCalendarUnit.Day, inUnit: NSCalendarUnit.WeekOfMonth, forDate: firstDateOfMonth())
+        let ordinalityOfFirstDay = Calendar.current.ordinality(of: .day, in: .weekOfMonth, for: firstDateOfMonth())
+        // 🔴修正前 for var i = 0; i < numberOfItems; i++ {
+        for i in 0 ..< numberOfItems {
+            // ②「月の初日」と「indexPath.item番目のセルに表示する日」の差を計算する
+            var dateComponents = DateComponents()
+            // 🔴修正前 (ordinalityOfFirstDay - 1)
+            dateComponents.day = i - (ordinalityOfFirstDay! - 1)
+            // ③ 表示する月の初日から②で計算した差を引いた日付を取得
+            // 🔴修正前 NSCalendar.currentCalendar().dateByAddingComponents(dateComponents, toDate: firstDateOfMonth(), options: NSCalendarOptions(rawValue: 0))!
+            let date = Calendar.current.date(byAdding: dateComponents as DateComponents, to: firstDateOfMonth() as Date)!
+            // ④配列に追加
+            // 🔴修正前 (date)
+            currentMonthOfDates.append(date as NSDate)
+        }
+    }
+    
+    // ⑵表記の変更
+    // 🔴修正前 (indexPath: NSIndexPath)
+    func conversionDateFormat(indexPath: IndexPath) -> String {
+        // 🔴修正前 (numberOfItems)
+        dateForCellAtIndexPath(numberOfItem: numberOfItems)
+        // 🔴修正前 NSDateFormatter = NSDateFormatter()
+        let formatter: DateFormatter = DateFormatter()
+        formatter.dateFormat = "d"
+        // 🔴修正前 return formatter.stringFromDate(currentMonthOfDates[indexPath.row])
+        return formatter.string(from: currentMonthOfDates[indexPath.row] as Date)
+    }
+    
+    
+    //前月の表示
+
+    func prevMonth(date: Date) -> Date {
+        currentMonthOfDates = []
+        selectedDate = date.monthAgoDate()
+        return selectedDate
+    }
+    
+    //次月の表示
+
+    func nextMonth(date: Date) -> Date {
+        currentMonthOfDates = []
+        selectedDate = date.monthLaterDate()
+        return selectedDate
+    }
+    
+    
+}
+
---
+++ Calendar2/Calendar2/ViewController.swift
@@ -0,0 +1,138 @@
+//
+//  ViewController.swift
+//  Calendar2
+//
+//  Created by 米田大弥 on 2019/01/30.
+//  Copyright © 2019 hiroya. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    //自作関数
+    class func lightBlue() -> UIColor {
+        //UIColorに色をつける
+        return UIColor(red: 92.0 / 255, green: 192.0 / 255, blue: 210.0 / 255, alpha: 1.0)
+    }
+    
+    //自作関数
+    class func lightRed() -> UIColor {
+        //UIColorに色をつける
+        return UIColor(red: 195.0 / 255, green: 123.0 / 255, blue: 175.0 / 255, alpha: 1.0)
+    }
+}
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    //ここ確認
+    let dateManager =  DateManager()
+    let daysPerWeek: Int = 7
+    let cellMargin: CGFloat = 2.0
+    var selectedDate = NSDate()
+    var today: NSDate!
+    let weekArray = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    
+    
+
+    @IBOutlet weak var headerPrevBtn: UIButton!
+    @IBOutlet weak var headerNextBtn: UIButton!
+    @IBOutlet weak var headerTitle: UILabel!
+    @IBOutlet weak var calenderHeaderView: UIView!
+    @IBOutlet weak var calenderCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //下3行コレクションビューを使うために必要
+        calenderCollectionView.delegate = self
+        calenderCollectionView.dataSource = self
+        calenderCollectionView.backgroundColor = UIColor.white
+        
+        //headerTitleを今の月にする
+        headerTitle.text = changeHeaderTitle(date: selectedDate)
+    }
+    
+    //1
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    //2
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // Section毎にCellの総数を変える.
+        if section == 0 {
+            return 7
+        } else {
+            return dateManager.daysAcquisition() //ここは月によって異なる
+        }
+    }
+
+    //3 fixをして as IndexPathを加えた
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CalendarCell
+        //テキストカラー
+        if (indexPath.row % 7 == 0) {
+            cell.textLabel.textColor = UIColor.lightRed()
+        } else if (indexPath.row % 7 == 6) {
+            cell.textLabel.textColor = UIColor.lightBlue()
+        } else {
+            cell.textLabel.textColor = UIColor.gray
+        }
+        //テキスト配置
+        if indexPath.section == 0 {
+            cell.textLabel.text = weekArray[indexPath.row]
+        } else {
+            cell.textLabel.text = dateManager.conversionDateFormat(indexPath: indexPath)
+//            cell.textLabel.text = weekArray[indexPath.row]
+            //月によって1日の場所は異なる(後ほど説明します)
+        }
+        
+        print(weekArray.count)
+
+        return cell
+    }
+    
+    //下3つが無いとsection1の土曜日が2行目になる。
+    //セルのサイズを設定
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numberOfMargin: CGFloat = 8.0
+        let width: CGFloat = (collectionView.frame.size.width - cellMargin * numberOfMargin) / CGFloat(daysPerWeek)
+        let height: CGFloat = width * 1.0
+        return CGSize(width: width, height: height)
+    }
+    
+    //セルの垂直方向のマージンを設定
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return cellMargin
+    }
+    
+    //セルの水平方向のマージンを設定
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return cellMargin
+    }
+
+    //headerの月を変更
+    func changeHeaderTitle(date: NSDate) -> String {
+        let formatter: DateFormatter = DateFormatter()
+        formatter.dateFormat = "M/yyyy"
+        let selectMonth = formatter.string(from: date as Date)
+        return selectMonth
+    }
+    
+    @IBAction func tappedHeaderPrevBtn(_ sender: UIButton) {
+        selectedDate = dateManager.prevMonth(date: selectedDate as Date) as NSDate
+        calenderCollectionView.reloadData()
+        headerTitle.text = changeHeaderTitle(date: selectedDate)
+        
+    }
+    
+    @IBAction func tappedHeaderNextBtn(_ sender: UIButton) {
+        
+        selectedDate = dateManager.nextMonth(date: selectedDate as Date) as NSDate
+        calenderCollectionView.reloadData()
+        headerTitle.text = changeHeaderTitle(date: selectedDate)
+    }
+    
+    
+}
+
